d413b82d40228ece15ea20686ea3e646fed60a70
Maciej Andrejczuk <maciej.andrejczuk@gmail.com>
2009-11-08 22:24:57 +0100

    Napisany worker, trzeba zrobic jeszcze common.c

--- manager.c
+++ manager.c
@@ -5,90 +5,81 @@
 #include <sys/wait.h>
 #include "err.h"
 
-void close_pipe(int* pipe) {
-	if (close(pipe[0]) == -1)
-		syserr("Error in close_pipe(%d)\n", pipe[0]);
-	if (close(pipe[1]) == -1)
-		syserr("Error in close_pipe(%d)\n", pipe[1]);		
-}
-
-void prepare_input(int* pipe) {
-	if (close(0) == -1)
-		syserr("Error in input.close(0)\n");
-	if (dup(pipe[0]) != 0)
-		syserr("Error in input.dup(pipe[0])\n");
-	close_pipe(pipe);
-}
-
-void prepare_output(int* pipe) {
-	if (close(1) == -1)
-		syserr("Error in output.close(0)\n");
-	if (dup(pipe[1]) != 1)
-		syserr("Error in output.dup(pipe[1])\n");
-	close_pipe(pipe);
-}
-
-void make_loop(int counter, int n, int* last_pipe, char* grammar) {
-	pid_t pid;
-	int pipe_dsc[2];
-	if (pipe(pipe_dsc) == -1) 
+void create_pipe(int* cur_pipe) {
+ 	if (pipe(cur_pipe) == -1)
 		syserr("Error in pipe()\n");
-	
-	if (counter == 1)
-		if (pipe(last_pipe) == -1) 
-			syserr("Error in pipe()\n");
-	
-	if (counter == n) {
-		prepare_output(last_pipe);
-		execl("./worker", grammar, NULL);
-		syserr("Error in execl()\n");
-	}
+}
 
+void close_pipe(int* cur_pipe) {
+	if (close(cur_pipe[0]) == -1)
+		syserr("Error in close_pipe(%d)\n", cur_pipe[0]);
+	if (close(cur_pipe[1]) == -1)
+		syserr("Error in close_pipe(%d)\n", cur_pipe[1]);
+}
+
+void prepare_input(int* cur_pipe) {
+	if (close(0) == -1)
+		syserr("Error in close(0)\n");
+	if (dup(cur_pipe[0]) != 0)
+		syserr("Error in dup(%s)\n", cur_pipe[0]);
+}
+
+void prepare_output(int* cur_pipe) {
+	if (close(1) == -1)
+		syserr("Error in close(1)\n");
+	if (dup(cur_pipe[1]) != 1)
+		syserr("Error in dup(%s)\n", cur_pipe[1]);
+}
+
+void make_loop(int n, int* last_pipe, char* grammar) {
+    // last_pipe[0] = input of the recently created pipe
+    // last_pipe[1] = output of the first pipe
+	pid_t pid;
+	if (n == 1) {
+		prepare_input(last_pipe);
+        prepare_output(last_pipe);
+        close_pipe(last_pipe);
+        return;
+	}
+	int next_pipe[2];
+    create_pipe(next_pipe);
 	switch (pid = fork()) {
 		case -1:
 			syserr("Error in fork()\n");
 		case 0:
-			printf("potomny, pid=%d, fork=%d\n", getpid(), pid);
-			prepare_input(pipe_dsc);
-			make_loop(counter + 1, n, last_pipe, grammar);
-			fatal("Error in make_loop %d\n",counter+1);
-
+		//	printf("potomny, pid=%d, fork=%d\n", getpid(), pid);
+			prepare_input(last_pipe);
+            close_pipe(last_pipe);
+            prepare_output(next_pipe);
+            close_pipe(next_pipe);
+			execl("./worker", "./worker", grammar, NULL);
+			syserr("Error in execl()\n");
 		default:
-			printf("macierzysty, pid=%d, fork=%d\n", getpid(), pid);
-			prepare_output(pipe_dsc);
-			if (counter > 1) {
-				close_pipe(last_pipe);
-				execl("./worker", grammar, NULL);
-				syserr("Error in execl()\n");
-			} else {
-				prepare_input(last_pipe);
-				return;
-			}	
+		//	printf("macierzysty, pid=%d, fork=%d\n", getpid(), pid);
+            last_pipe[0]=next_pipe[0];
+            make_loop(n-1, last_pipe, grammar);
+            return;
 	}
 }
 
-void work() {
-	int ile = '0';
-	write(1, &ile, sizeof(int));
-	read(0, &ile, sizeof(int));
-	write(2, &ile, sizeof(int));
-}
 
 int main(int argc, char* argv[]) {
-	int n;
+	int n,i;
 	int last_pipe[2];
 	if (argc != 4) {
-		fatal("Usage: %s <number of workers> <grammar file> <starting symbol>\n");
+		fatal("Usage: %s <workers number> <grammar file> <starting symbol>\n",
+            argv[0]);
 	}
 	n = atoi(argv[1]);
 	if (n <= 0) {
 		fatal("Number of workers should be a positive integer");
 	}
-	make_loop(1, n, last_pipe, argv[2]);
-	work();
-//	for (i=1; i<n; i++) {
-//		if (wait(0) == -1) syserr("Error in wait()\n");
-//	}
+    create_pipe(last_pipe);
+	make_loop(n, last_pipe, argv[2]);
+	write(1, argv[3], sizeof(int));
+	for (i=1; i<n; i++) {
+		if (wait(0) == -1) syserr("Error in wait()\n");
+	}
 	return 0;
 }
 
--- worker.c
+++ worker.c
@@ -1,17 +1,164 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <err.h>
 
+#define MAX_BUF_SIZE 65536
+// 64 Kb > 4Kb = minimal pipe capacity
+#define GRAMMAR_LENGTH 20
+#define PRODUCTION_LENGTH 20
+#define MAX_LINE_LEN 100
+#define FINISHING '!'
+#define CLOSING '$'
+
+struct grammar {
+    int len;
+    char prod[GRAMMAR_LENGTH][PRODUCTION_LENGTH];
+};
+
+void read_message(char* text) {
+    int length;
+	read(0, &length, sizeof(int));
+    read(0, text, length);
+}
+
+void write_message(char* text) {
+    int length = strlen(text);
+    write(1, &length, sizeof(int));
+	write(1, text, length);
+}
+
+void debug(char *text) {
+    int length = strlen(text);
+    write(2, text, length);
+}
+
+void debug_int(int n) {
+    if (n == 0) debug("0");
+    char c[10];
+    int k = 0;
+    while (n > 0) {
+        c[k] = '0' + (n % 10);
+        n /= 10;
+        k++;
+    }
+    c[k] = '\0';
+    debug(c);
+}
+
+void read_grammar(struct grammar* g, char* filename) {
+    char line[MAX_LINE_LEN + 3];
+    int len, counter = 0;
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        fatal("Failed to open a file with grammar: %s\n", filename);
+    }
+    while (1) {
+        fgets(line, MAX_LINE_LEN, file);
+        len = strlen(line);
+        if (len == 0) break;
+        strncpy(line, g->prod[counter], MAX_LINE_LEN);
+        counter++;
+    }
+    g->len = counter;
+    fclose(file);
+}
+
+int is_upper(char c) {
+    return (c >= 'A' && c <= 'Z');
+}
+
+int is_finishing(char* text) {
+    int len, i;
+    if (text[0] == FINISHING) return 1;
+    len = strlen(text);
+    for (i = 0; i < len; i++) {
+        if (is_upper(text[i])) return 0;
+    }
+    return 1;
+}
+
+int is_closing(char* text) {
+    return (text[0] == CLOSING);
+}
+
+void generate_word(struct grammar *g, char* text) {
+    int i = 0, j = 0, k = 0;
+    int len = strlen(text);
+    int prod_len;
+    // Searching first nonterminal
+    while (k < len) {
+        if (is_upper(text[k])) break;
+        k++;
+    }
+    if (k == len) {
+        fatal("Nonterminal not found as expected.\n");
+    }
+    // Searching first production for this nonterminal
+    while (j < g->len) {
+        if (g->prod[j][0] == text[k]) break;
+        j++;
+    }
+    if (j == g->len) {
+        fatal("No production for nonterminal %c found\n", text[k]);
+    }
+    // Inserting production into text
+    prod_len = strlen(g->prod[j]) - 1;
+    if (prod_len > 1) {
+        for (i = len; i > k; i--) {
+            text[i + prod_len - 1] = text[i];
+        }
+    }
+    for (i = 1; i <= prod_len; i++) {
+        text[k + i - 1] = g->prod[j][i];
+    }
+}
+
+int work(struct grammar* g, int is_manager) {
+    char text[MAX_BUF_SIZE];
+    int length;
+    while (1) {
+        debug("Process ");
+        debug_int(getpid());
+        debug(": ");
+        read_message(text);
+
+        if (is_finishing(text)) {
+            if (is_manager == 0) {
+                write_message(text);
+                debug("Work finished, waiting for close command.\n");
+            } else {
+                text[0] = CLOSING;
+                write_message(text);
+                debug("Work finished, sending close command.\n");
+            }
+            continue;
+        }
+        if (is_closing(text)) {
+            if (is_manager == 0) {
+                write_message(text);
+            }
+            debug("Close command recieved, exiting.\n");
+            close(0);
+            close(1);
+            break;
+        }
+        generate_word(g, text);
+        debug("Sending forward: ");
+        debug(text);
+        debug("\n");
+        write_message(text);
+    }
+}
+
 int main(int argc, char* argv[]) {
-	int ile;
-	read(0, &ile, sizeof(int));
-	write(2, &ile, sizeof(int));
-	ile++;
-	write(1, &ile, sizeof(int));
-	wait(0);
-	return 0;
+	struct grammar g;
+    if (argc != 2) {
+        fatal("Usage: %s <grammar file>\n", argv[0]);
+    }
+    read_grammar(&g, argv[1]);
+    work(&g, 0);
+   	return 0;
 }
 
